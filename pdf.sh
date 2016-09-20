d247eaf033b2703d70308e30db47515ed664ba28
Andre Restivo <andre.restivo@gmail.com>
2016-09-20 02:35:27 +0100

    Experimental PDF generation

---
+++ pdf.sh
@@ -0,0 +1,17 @@
+PRINCE='/home/arestivo/Apps/prince/bin/prince'
+cd pdf
+rm *.html
+rm *.pdf
+for f in ../markdown/*
+do
+ name=$(echo "$f" | sed "s/.*\///" | sed "s/\..*//")
+ phantomjs save_page.js "http://www.fe.up.pt/~arestivo/presentation/$name/" > "$name.html"
+done
+
+for f in *.html
+do
+ name=$(echo "$f" | sed "s/.*\///" | sed "s/\..*//")
+ $PRINCE --media print --page-size A4 "$name.html" "output/$name.pdf"
+ rm "$name.html"
+done
+((rsync --progress -r output/ arestivo@pinguim:~/public_html/pdf && notify-send 'FEUP Sync' 'Success') || notify-send -u critical 'FEUP Sync' 'Failed')
