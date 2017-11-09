f74a034bca3f4023f7c0c4f6833ab676ced341bc
Andre Restivo <andre.restivo@gmail.com>
2017-11-09 09:31:10 +0000

    Ignore errors when generating PDF

--- pdf/save_page.js
+++ pdf/save_page.js
@@ -1,6 +1,9 @@
 var system = require('system');
 var page = require('webpage').create();
 
+page.onError = function(msg, trace) {
+};
+
 page.open(system.args[1], function()
 {
     console.log(page.content);
