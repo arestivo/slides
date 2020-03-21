b2f627749d35f6b925cca81ae117c25fd3837405
Andre Restivo <andre.restivo@gmail.com>
2020-03-21 15:23:53 +0000

    CSS laser

--- script/laser-pointer.js
+++ script/laser-pointer.js
@@ -1,11 +1,5 @@
 const state = { laser : false, paragraph : false }
 
-const laserListener = (event) => {
-  const svg = document.querySelector('svg.pointer')
-  svg.style.left = `${event.clientX - 5}px`
-  svg.style.top = `${event.clientY - 5}px`
-}
-
 const paragraphListener = (event) => {
   if (event.type == 'mouseenter')
     event.target.style.color = null
@@ -38,39 +32,13 @@ const paragraphFocusOff = () => {
 }
 
 const laserOn = (event) => {
-  document.body.setAttribute('style', 'cursor: none;')
-
-  const svg = document.createElementNS('http://www.w3.org/2000/svg', 'svg')
-  svg.setAttributeNS('http://www.w3.org/2000/xmlns/', 'xmlns:xlink', 'http://www.w3.org/1999/xlink')
-  svg.classList.add('pointer')
-  svg.setAttribute('style', 'z-index: 20; position: absolute; width: 10px; height: 10px; filter: blur(1px);')
-  svg.setAttribute('preserveAspectRatio', 'xMidYMid meet')
-  svg.setAttribute('viewBox', '0 0 10 10')
-  
-  const pointer = document.createElementNS('http://www.w3.org/2000/svg', 'circle')
-  pointer.setAttributeNS(null, 'cx', 5)
-  pointer.setAttributeNS(null, 'cy', 5)
-  pointer.setAttributeNS(null, 'r', 5)
-  pointer.setAttributeNS(null, 'style', 'position:absolute; fill: red; stroke: red; stroke-width: 1px' )
-  
-  document.body.appendChild(svg)
-  svg.appendChild(pointer)
-  
-  pointer.addEventListener('click', laserOff) 
-
-  document.addEventListener('mousemove', laserListener)
+  document.body.classList.add('laser')
 
   state.laser = true
-
-  if (event) event.preventDefault()
 }
 
 const laserOff = () => {
-  document.body.removeAttribute('style')
-  const svg = document.querySelector('svg.pointer')
-
-  if (svg) document.body.removeChild(svg)
-  document.removeEventListener('mousemove', laserListener)
+  document.body.classList.remove('laser')
 
   state.laser = false
 }
